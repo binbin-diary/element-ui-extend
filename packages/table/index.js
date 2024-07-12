d42979e6c1cd116b093e746de88a18311cfbf0ff
wubinbin <binbin.wu@gientech.com>
2024-07-12 16:14:05 +0800

    修改

--- packages/table/index.js
+++
@@ -1,7 +0,0 @@
-import DebounceButton from './src/debounce-botton.vue';
-
-DebounceButton.install = (Vue) => {
-    Vue.component(DebounceButton.name, DebounceButton);
-};
-
-export default DebounceButton;
