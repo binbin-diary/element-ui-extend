c3aff74160e2da757e1c360daa07a3b671a48e77
wubinbin <binbin.wu@gientech.com>
2024-07-12 15:29:41 +0800

    修改

---
+++ packages/table/src/debounce-botton.vue
@@ -0,0 +1,34 @@
+<script>
+import debounce from 'lodash/debounce';
+
+export default {
+  name: 'DebounceButton',
+  props: {
+    delay: {
+        type: Number,
+        default: 300,
+    },
+  },
+  methods: {
+    handlerClick(e) {
+      this.$emit('click', e);
+    },
+  },
+  render() {
+    return (
+      <el-button
+        {...{
+          props: this.$props,
+          attrs: this.$attrs,
+          on: {
+            ...this.$listeners,
+            click: debounce(this.handlerClick, this.$props.delay),
+          },
+        }}
+      >
+        {this.$slots.default}
+      </el-button>
+    );
+  },
+};
+</script>
